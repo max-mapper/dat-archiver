a2220e8adb520df571a468606ef0e35278c7386b
Joe Hand <joe@joeahand.com>
2016-10-19 13:25:14 -0700

    use hypercore-archiver

--- index.js
+++ index.js
@@ -2,22 +2,23 @@ var path = require('path')
 var crypto = require('crypto')
 var pump = require('pump')
 var mkdirp = require('mkdirp')
-var Dat = require('dat-js')
 var peerNetwork = require('peer-network')
+var coreArchive = require('hypercore-archiver')
 
 module.exports = Archiver
 
 function Archiver (options) {
   if (!(this instanceof Archiver)) return new Archiver(options)
   var self = this
-  
+
   if (!options) options = {}
   if (!options.dir) throw new Error('must specify directory to store dats')
   self.options = options
-  self.peers = {}
   self.servers = {}
+  self.network = peerNetwork()
 
-  this.network = peerNetwork()
+  mkdirp.sync(path.join(options.dir, 'data'))
+  self.archives = coreArchive(options.dir)
 }
 
 Archiver.prototype.join = function (key) {
@@ -26,24 +27,20 @@ Archiver.prototype.join = function (key) {
   if (self.servers[key]) throw new Error('already joined that key')
   var server = this.servers[key] = self.network.createServer()
   server.on('connection', function (stream) {
-    console.log('new connection')
+    console.log('new connection on:', key)
     readKey()
     function readKey () {
       var datKey = stream.read(32)
       if (!datKey) return stream.once('readable', readKey)
-      datKey = datKey.toString('hex')
-      var datDir = path.join(self.options.dir, datKey)
-      mkdirp.sync(datDir)
-      var dat = self.peers[datKey] = Dat({dir: datDir, discovery: false, key: datKey})
-      dat.on('ready', function () {
-        console.log('dat ready')
-        var replicator = dat.archive.replicate()
-        pump(stream, replicator, stream, function (err) {
+      // datKey = encoding.encode(datKey)
+      self.archives.add(datKey, function (err) {
+        if (err) throw err
+        console.log('starting replication', datKey.toString('hex'))
+        pump(stream, self.archives.replicate(), stream, function (err) {
           if (err) throw err
-          dat.close()
-          console.log('done')
+          console.log('done replicating')
         })
-      }) 
+      })
     }
   })
   server.listen(key)
